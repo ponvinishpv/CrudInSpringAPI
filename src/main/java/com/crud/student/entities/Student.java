a50c033b1e7bdb12a46d6b2efff3dff3efe9301d
ponvinishpv <151833513+ponvinishpv@users.noreply.github.com>
2023-11-23 04:26:42 -0800

    db configured, created entity

---
+++ src/main/java/com/crud/student/entities/Student.java
@@ -0,0 +1,47 @@
+package com.crud.student.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Student_tbl")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column()
+    private String name;
+
+    @Column()
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
